f9a9f7839c7561ce210b076a588d0b0e1c793a71
zhongchaofang <Zhong_CF@163.com>
2023-12-17 21:14:11 +0800

    Add files via upload

---
+++ protest/protest.sh
@@ -0,0 +1,3 @@
+java -jar /data5/zcf/tool/prottest-3.4.2/prottest-3.4.2.jar -i single_copy_combine_muscle_out278-gb.phy -S 0 -all-distributions -F -AIC -BIC -tc 0.5 -threads 50 > prottestoutput2
+echo prottest.out end time is `date`
+echo protest.sh end time is `date`
