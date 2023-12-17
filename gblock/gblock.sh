291610f40a54695b948a79158d348009b6ae1434
zhongchaofang <Zhong_CF@163.com>
2023-12-17 21:41:58 +0800

    Add files via upload

---
+++ gblock/gblock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/data1/zhongchaofang/Gblocks_0.91b/Gblocks single_copy_combine_muscle_out278 -t=p
+echo Gblocks single_copy_combine_muscle_out278 end time is `date`
+echo gblock.sh end time is `date`
